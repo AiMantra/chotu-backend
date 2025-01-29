29496e2ec7dbbc415f9b47399dacb9ca3e726666
vandita27 <112273675+vandita27@users.noreply.github.com>
2025-01-29 11:24:08 +0530

    new fields

--- ecom/models.py
+++ ecom/models.py
@@ -40,7 +40,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-dISCOUNT, GST 
+# dISCOUNT, GST 
 # Products under categories
 class Product(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
@@ -48,6 +48,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
     stock = models.IntegerField(default=0)
+    discount = models.CharField(max_length=250, null=False, blank=False)
+    gst = models.CharField(max_length=250, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
